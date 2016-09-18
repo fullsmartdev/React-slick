4fa5156f82309563b43229c5decd117af3f71c41
hideaki.chain@gmail.com <senior.dev.software@gmail.com>
2016-09-18 14:42:48 +0530

    Fixes to customPaging default props

--- src/mixins/event-handlers.js
+++ src/mixins/event-handlers.js
@@ -89,9 +89,6 @@ var EventHandlers = {
     if (this.state.animating) {
       return;
     }
-    if (this.props.vertical && this.props.swipeToSlide && this.props.verticalSwiping) {
-      e.preventDefault();
-    }
     var swipeLeft;
     var curLeft, positionOffset;
     var touchObject = this.state.touchObject;
