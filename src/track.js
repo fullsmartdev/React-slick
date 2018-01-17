8ab297d67c4ae75fe51ef644bdb5e79a5bdbb43b
hideaki.chain@gmail.com <senior.dev.software@gmail.com>
2018-01-17 16:23:29 +0530

    added new script to automated detail fetching of jquery slick at some state

--- src/track.js
+++ src/track.js
@@ -39,12 +39,8 @@ var getSlideStyle = function (spec) {
   }
 
   if (spec.fade) {
-    style.position = 'relative'; console.log(spec);
-    if (spec.vertical) {
-      style.top = -spec.index * spec.slideHeight;
-    } else {
-      style.left = -spec.index * spec.slideWidth;
-    }
+    style.position = 'relative';
+    style.left = -spec.index * spec.slideWidth;
     style.opacity = (spec.currentSlide === spec.index) ? 1 : 0;
     style.transition = 'opacity ' + spec.speed + 'ms ' + spec.cssEase;
     style.WebkitTransition = 'opacity ' + spec.speed + 'ms ' + spec.cssEase;
